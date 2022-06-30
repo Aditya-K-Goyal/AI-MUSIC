af3116dd80a7a1386966d9a744f3de84565690e4
Aditya K Goyal <101977885+Aditya-K-Goyal@users.noreply.github.com>
2022-06-30 14:35:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 song_1 ="";
 song_2 ="";
 peterpan_song="";
+harrypotter_song="";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreLeftWristY =0;
+scoreRightWristY =0;
 function preload()
 {
     song_1 = loadSound("music.mp3");
@@ -24,6 +26,8 @@ function gotPoses(results){
     if(results.length >0){
         console.log(results);
         scoreLeftWristY = results[0].pose.keypoints[9].score;
+        scoreRightWristY = results[0].pose.keypoints[10].score;
+        console.log("score Left Wrist Y = " +scoreLeftWristY + " , score Right Wrist Y = " + scoreRightWristY);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " , leftWristY = " + leftWristY);
@@ -48,6 +52,19 @@ function draw()
         }
         else{
             document.getElementById("song_name").innerHTML ="Song Name :Peter Pan Song";
+        }
+    }
+    harrypotter_song = song_1.isPlaying();
+    console.log(harrypotter_song);
+    if(scoreRightWristY > 0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        song_2.stop();
+        if(harrypotter_song == false){
+            song_1.play();
+        }
+        else{
+            document.getElementById("song_name").innerHTML = "Song Name :Harry Potter Theme Song";
         }
     }
 }
